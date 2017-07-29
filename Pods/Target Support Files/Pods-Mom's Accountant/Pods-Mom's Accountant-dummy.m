d7a1856867a6173724c73337a38d21efa599e1b1
Calvin Aquino <Calvin.Aquino@logmein.com>
2017-07-28 22:58:36 -0300

    Impemented Firebase - firebase database - using cocoapods

---
+++ Pods/Target Support Files/Pods-Mom's Accountant/Pods-Mom's Accountant-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mom_s_Accountant : NSObject
+@end
+@implementation PodsDummy_Pods_Mom_s_Accountant
+@end
---
+++ Pods/Target Support Files/Pods-Mom's Accountant/Pods-Mom's Accountant-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Mom_s_AccountantVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Mom_s_AccountantVersionString[];
+
